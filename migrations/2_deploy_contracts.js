3a520064733e93f3d4f5c0d9038cd5f632ef0d41
Ayushi Ranjan <ayushiranjan14@gmail.com>
2017-04-28 07:40:58 +0530

    Proerly indented migrations code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,5 @@
-//var ConvertLib = artifacts.require("./ConvertLib.sol");
-//var MetaCoin = artifacts.require("./MetaCoin.sol");
-//var User = artifacts.require("./User.sol");
-//var Approval = artifacts.require("./Approval.sol");
-//var PaymentCards = artifacts.require("./PaymentCards.sol");
 var Transactions = artifacts.require("./Transactions.sol");
 
 module.exports = function(deployer) {
-  //deployer.deploy(User);
-   //deployer.deploy(Approval);
-	//deployer.deploy(PaymentCards);
 	deployer.deploy(Transactions);
 };
